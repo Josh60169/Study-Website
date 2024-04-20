e524567e958122d909fd2b89cdb0f49054228c6a
Josh60169 <yashvihangupta@gmail.com>
2024-04-20 16:03:50 -0500

    added most of the functionality of the practice button and added a practice sessison (needs a next button)

--- index2.js
+++ index2.js
@@ -1,5 +1,7 @@
 let questionList = [];
 let answerList = [];
+let correctAns = 0;
+let questionNumber = 0;
 
 $(document).ready(() => {
     $("#add-btn").click((e) => {
@@ -9,6 +11,36 @@ $(document).ready(() => {
         answerList.push($("#answer-txtbox").val())
         renderList();
     });
+
+    $("#practice-btn").click(function(e) {
+        e.preventDefault();
+
+        $("#main-div-1").fadeOut();
+        $("#main-div-2").fadeIn();
+
+        $("#question-p").text(questionList[questionNumber]);
+    });
+
+
+    $("#check-btn").click(function(e) {
+        e.preventDefault();
+
+        let userAns = $("#userAnswer-txtbox").val();
+
+        if (userAns == answerList[questionNumber]) {
+            correctAns++;
+            $("#answer-p").text("Correct!");
+        } else
+            $("#answer-p").text(`Incorrect. The right answer was ${answerList[questionNumber]}`);
+        
+        questionNumber++;
+
+        if (questionNumber !== questionList.length)
+            $("#question-p").text(questionList[questionNumber]);
+        
+    });
+
+        
 });
 
 const renderList = () => {
