f66050e70986305c0376f2223295781ffc8d2777
Josh60169 <yashvihangupta@gmail.com>
2024-04-20 14:20:55 -0500

    added some functionality to the add button (not complete)

--- index2.js
+++ index2.js
@@ -1,4 +1,18 @@
-$("add-btn").click(e => {
-    
+let questionList = [];
 
+$(document).ready(() => {
+    $("#add-btn").click((e) => {
+        e.preventDefault();
+
+        questionList.push($("#question-txtbox").val());
+        renderList();
+    });
 });
+
+const renderList = () => {
+    $("#ul-questions").empty();
+
+    questionList.forEach((val) => {
+        $("#ul-questions").append(`<li>${val}</li>`);
+    });
+};
