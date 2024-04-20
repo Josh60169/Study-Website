c37a366520b93747287a54352e9b6e0f016ef159
Josh60169 <yashvihangupta@gmail.com>
2024-04-20 14:37:44 -0500

    added more functionality to add button

--- index2.js
+++ index2.js
@@ -1,18 +1,25 @@
 let questionList = [];
+let answerList = [];
 
 $(document).ready(() => {
     $("#add-btn").click((e) => {
         e.preventDefault();
 
         questionList.push($("#question-txtbox").val());
+        answerList.push($("#answer-txtbox").val())
         renderList();
     });
 });
 
 const renderList = () => {
     $("#ul-questions").empty();
+    $("#ul-answers").empty();
 
     questionList.forEach((val) => {
         $("#ul-questions").append(`<li>${val}</li>`);
+    });
+
+    answerList.forEach((val) => {
+        $("#ul-answers").append(`<li>${val}</li>`);
     });
 };
